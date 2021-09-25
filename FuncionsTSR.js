11d793d089cc4d604d8531533d859ab9781e158d
Andreu Mut Portes <amutpor@upv.edu.es>
2021-09-25 14:49:25 +0200

    Modificacio TSR Funcions

--- FuncionsTSR.js
+++ FuncionsTSR.js
@@ -1,3 +1,4 @@
+//Exercici amb Funcions imperatives
 function sumScan(a){
     let b=[]
     let acumulador = 0
@@ -46,7 +47,58 @@ function minScan(a){
 }
 
 let v=[2,3,4,1]
+console.log("Exemples imperatius:")
 console.log("SumScan: "+ sumScan(v))
 console.log("PrdScan: "+ prdScan(v))
 console.log("MaxScan: "+ maxScan(v))
 console.log("MinScan: "+ minScan(v))
+
+console.log()
+
+//Exercici anterior amb funcions d'alt ordre
+function sumScan2(a){
+    let b=[]
+    let acumulador = 0
+    a.map((x)=>{
+        acumulador += x
+        b.push(acumulador)
+    })
+    return b
+}
+
+function prdScan2(a){
+    let b=[]
+    let acumulador = 1
+    a.map((x)=>{
+        acumulador *= x
+        b.push(acumulador)
+    })
+    return b
+}
+
+function maxScan2(a){
+    let b=[]
+    let max = a[0]
+    a.map((x)=>{
+        if (x > max){max = x}
+        b.push(max)
+    })
+    return b
+}
+
+function minScan2(a){
+    let b=[]
+    let min = a[0]
+    a.map((x)=>{
+        if(x < min){min = x}
+        b.push(min)
+    })
+    return b
+}
+
+
+console.log("Exemples funcionals:")
+console.log("SumScan2: "+ sumScan2(v))
+console.log("PrdScan2: "+ prdScan2(v))
+console.log("MaxScan2: "+ maxScan2(v))
+console.log("MinScan2: "+ minScan2(v))
